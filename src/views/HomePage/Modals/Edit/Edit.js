9cc8418f53d9ac19f080fc0aaf979392e8c4494b
florentbekteshi <florentbekteshii@gmail.com>
2022-02-23 13:56:38 -0800

    input

--- src/views/HomePage/Modals/Edit/Edit.js
+++
@@ -1,9 +0,0 @@
-export default{
-  props:["row"],
-  data(){
-    return{
-
-    }
-  },
-
-}
--- src/views/HomePage/Modals/Edit/index.vue
+++
@@ -1,3 +0,0 @@
-<template src="./Edit.html"></template>
-<script src="./Edit.js"></script>
-<style scoped src="./Edit.css"></style>
