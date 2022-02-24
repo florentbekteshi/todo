1260568588388a6caf3042a97cdf7a74994961d1
florentbekteshi <florentbekteshii@gmail.com>
2022-02-24 09:20:01 -0800

    fixed modal and errors fixed

--- src/views/HomePage/Modals/Add/Add.js
+++ src/views/HomePage/Modals/Add/Add.js
@@ -1,44 +1,61 @@
-export default{
-  props:{
-    row:{
+export default {
+  props: {
+    row: {
       required: true
     },
-    editMode:{
+    editMode: {
       required: true
     },
-   
+
   },
-  data(){
-    return{
-      data:{
-        email:null,
+  data() {
+    return {
+      data: {
+        email: null,
         name: null,
         gender: null,
-        
+
+      },
+      errors: {
+        email: false,
+        name: false,
+        gender: false
       }
     }
   },
-  methods:{
-    addRow(){
-     if(this.data.name == null || this.data.name == ''){
-       
-       document.getElementById('nameInput').style.border = "1px solid red";
-       return;
-     }
-     if(this.data.email == null || this.data.email == ''){
-      document.getElementById('emailInput').style.border = "1px solid red";
-       return;
+  mounted() {
+    this.data = [];
+    if(this.row === undefined || this.row.length == 0) {
+      console.log(this.editMode)
+    } else {
+      this.data = this.row
     }
-    if(this.data.gender == null || this.data.gender == ''){
-      document.getElementById('gender').style.border = "1px solid red";
-       return;
-    }
-    this.$emit('addRow', this.data)
-    this.$emit('toggleFunc');
-    
+  },
+  methods: {
+    addRow() {
+      if (this.data.name == null || this.data.name == '') {
+        this.errors.name = true;
+      } else {
+        this.errors.name = false;
+      }
 
+      if (this.data.email == null || this.data.email == '') {
+        this.errors.email = true;
+      }else{
+        this.errors.email = false;
+      }
+
+      if (this.data.gender == null || this.data.gender == '') {
+        this.errors.gender = true;
+      }else{
+        this.errors.gender = false;
+      }
+      if(!this.errors.name && !this.errors.email && !this.errors.gender){
+        this.$emit('addRow', this.data)
+        this.$emit('toggleFunc');
+      }
     },
-    editRow(){
+    editRow() {
       this.$emit('editRow', this.row)
       this.$emit('toggleFunc');
     }
